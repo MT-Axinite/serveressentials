f8c5bca504080fcee7fff410dc004add0b762767
sporaxfr <The.Sporax@gmail.com>
2017-09-03 19:56:44 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -11,24 +11,17 @@ players = {}
 check_timer = 0
 dofile(minetest.get_modpath("serveressentials") .. "/settings.lua")
 
-minetest.register_privilege("godmode",
-		"Player can use godmode with the /godmode command")
 minetest.register_privilege("broadcast",
 		"Player can use /broadcast command")
-minetest.register_privilege("kill",
-		"Player can kill other players with the /kill command")
+minetest.register_privilege("top",
+		"Player can use the /top command")
 minetest.register_privilege("whois",
 		"Player can view other player's network information with the /whois command")
 
-minetest.register_chatcommand("clearinv", {
-	params = "";
-	description = "Clear your inventory",
-	privs = {},
-	func = function(player_name, text)
-		local inventory = minetest.get_player_by_name(player_name):get_inventory()
-		inventory:set_list("main", {})
-	end,
-})
+if AFK_CHECK then
+	minetest.register_privilege("canafk",
+		"Player can remain afk without being kicked")
+end
 
 minetest.register_chatcommand("broadcast", {
 	params = "<text>",
@@ -39,21 +32,26 @@ minetest.register_chatcommand("broadcast", {
 		return
 	end,
 })
-
-minetest.register_chatcommand("kill", {
-	params = "<player_name>",
-	description = "kill specified player",
-	privs = {kill = true},
+minetest.register_chatcommand("top", {
+	params = "",
+	description = "Teleport to topmost block at your current position",
+	privs = {top = true},
 	func = function(player_name, param)
+		curr_pos = minetest.get_player_by_name(player_name):getpos()
+		curr_pos["y"] = math.ceil(curr_pos["y"]) + 0.5
 
-		if #param==0 then
-			minetest.chat_send_player(player_name, "You must supply a player name")
-		elseif players[param] then
-			minetest.chat_send_player(player_name, "Killing player " .. param)
-			minetest.get_player_by_name(param):set_hp(0)
-		else
-			minetest.chat_send_player(player_name, "Player " .. param .. " cannot be found")
+		while minetest.get_node(curr_pos)["name"] ~= "ignore" do
+			curr_pos["y"] = curr_pos["y"] + 1
 		end
+
+		curr_pos["y"] = curr_pos["y"] - 0.5
+
+		while minetest.get_node(curr_pos)["name"] == "air" do
+			curr_pos["y"] = curr_pos["y"] - 1
+		end
+		curr_pos["y"] = curr_pos["y"] + 0.5
+
+		minetest.get_player_by_name(player_name):setpos(curr_pos)
 		return
 	end
 })
@@ -64,21 +62,6 @@ minetest.register_chatcommand("killme", {
 	func = function(player_name, param)
 		minetest.chat_send_player(player_name, "Killing Player " .. player_name)
 		minetest.get_player_by_name(player_name):set_hp(0)
-		return
-	end
-})
-
-minetest.register_chatcommand("godmode", {
-	params = "",
-	description = "Toggle godmode",
-	privs = {godmode = true},
-	func = function(player_name, param)
-		players[player_name]["god_mode"] = not players[player_name]["god_mode"];
-		if players[player_name]["god_mode"] then
-			minetest.chat_send_player(player_name, "Godmode is now on")
-		else
-			minetest.chat_send_player(player_name, "Godmode is now off")
-		end
 		return
 	end
 })
@@ -133,5 +116,45 @@ minetest.register_on_chat_message(function(name, message)
 		return true
 	end
 	return
+end)
+
+minetest.register_globalstep(function(dtime)
+	-- Loop through all connected players
+	for _,player in ipairs(minetest.get_connected_players()) do
+		local player_name = player:get_player_name()
+
+		-- Only continue if the player has an entry in the players table
+		if players[player_name] then
+
+			-- Check for afk players
+			if AFK_CHECK and not minetest.check_player_privs(player_name, {canafk=true}) then
+				check_timer = check_timer + dtime
+				if check_timer > AFK_CHECK_INTERVAL then
+					check_timer = 0
+
+					-- Kick player if he/she has been inactive for longer than MAX_INACTIVE_TIME seconds
+					if players[player_name]["last_action"] + MAX_AFK_TIME <
+							minetest.get_gametime() then
+						minetest.kick_player(player_name, "Kicked for inactivity")
+					end
+
+					-- Warn player if he/she has less than WARN_TIME seconds to move or be kicked
+					if players[player_name]["last_action"] + MAX_AFK_TIME - AFK_WARN_TIME <
+							minetest.get_gametime() then
+						minetest.chat_send_player(player_name, "Warning, you have " ..
+								tostring(players[player_name]["last_action"] + MAX_AFK_TIME - minetest.get_gametime())
+								.. " seconds to move or be kicked")
+					end
+				end
+
+				-- Check if this player is doing an action
+				for _,keyPressed in pairs(player:get_player_control()) do
+					if keyPressed then
+						players[player_name]["last_action"] = minetest.get_gametime()
+					end
+				end
+			end
+		end
+	end
 end)
 
